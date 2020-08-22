074c4a637445ab7455d9ea4c6ca6eb9d8d460b61
bashangbasir <38761135+bashangbasir@users.noreply.github.com>
2020-08-23 01:22:26 +0800

    Add chapter3 exercise.

--- src/chapter3/ChangeToRM1.java
+++ src/chapter3/ChangeToRM1.java
@@ -2,7 +2,7 @@ package chapter3;
 
 import java.util.Scanner;
 
-class ChangeToRM1 {
+public class ChangeToRM1 {
     public static void main(String[] args) {
         // Get 5cent,10cent, 20cent,50cents
         Scanner scanner = new Scanner(System.in);
@@ -15,15 +15,13 @@ class ChangeToRM1 {
         System.out.println("Enter the 50 cents quantities:");
         int _50cents = Integer.parseInt(scanner.nextLine());
         //Calculate if the sum of value user fill in equal to 1, more or less than 1
-        double sum = (_5cents*0.05)+(_10cents*0.1)+(_20cents*0.2)+(_50cents*0.5);
+        double sum = (_5cents * 0.05) + (_10cents * 0.1) + (_20cents * 0.2) + (_50cents * 0.5);
 
-        if (sum > 1){
-            System.out.println("You exceed RM1 by RM"+(sum-1));
-        }
-        else if (sum<1){
-            System.out.println("You short RM1 by RM"+(1-sum));
-        }
-        else {
+        if (sum > 1) {
+            System.out.println("You exceed RM1 by RM" + (sum - 1));
+        } else if (sum < 1) {
+            System.out.println("You short RM1 by RM" + (1 - sum));
+        } else {
             System.out.println("You enter RM1, you win!");
         }
 
